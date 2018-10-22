a0e5dbb360a02f4bacd69b0127e69b0242e9842d
BurbanoJaime <burbanojaime98@gmail.com>
2018-10-22 14:51:24 -0500

    project uploaded

---
+++ discs.js
@@ -0,0 +1,69 @@
+module.exports = [
+    {
+        name: "ONE MORE LIGHT LIVE",
+        img: "data/discs/images/one_more_light_live.jpg",
+        type: "ALBUM",
+        /*
+        songs: [
+            {
+                name: "ONE MORE LIGHT",
+                song:  "data/discs/images/one_more_light_live/one_more_light.mp3",
+            },
+            {
+                name: "TALKING TO MYSELF",
+                song:  "data/discs/images/one_more_light_live/talking_to_myself.mp3",
+            },
+        ],
+        */
+        price: 21.95,
+    },
+    {
+        name: "CASTLE OF GLASS",
+        img: "data/discs/images/castle_of_glass.jpg",
+        type: "SINGLE",
+        //song: "data/discs/songs/castle_of_glass.mp3",
+        price: 4.25,
+    },
+    {
+        name: "IRIDESCENT",
+        img: "data/discs/images/iridiscent.jpg",
+        type: "SINGLE",
+        //song: "data/discs/songs/castle_of_glass.mp3",
+        price: 3.99,
+    },
+    {
+        name: "LIVING THINGS",
+        img: "data/discs/images/living_things.jpg",
+        type: "ALBUM",
+        //song: "data/discs/songs/castle_of_glass.mp3",
+        price: 15.50,
+    },
+    {
+        name: "METEORA",
+        img: "data/discs/images/meteora.jpg",
+        type: "ALBUM",
+        //song: "data/discs/songs/castle_of_glass.mp3",
+        price: 18.99,
+    },
+    {
+        name: "FINAL MASQUERADE",
+        img: "data/discs/images/final_masquerade.jpg",
+        type: "SINGLE",
+        //song: "data/discs/songs/castle_of_glass.mp3",
+        price: 5.50,
+    },
+    {
+        name: "MINUTES OF MIDNIGHT",
+        img: "data/discs/images/minutes_of_midnight.jpg",
+        type: "ALBUM",
+        //song: "data/discs/songs/castle_of_glass.mp3",
+        price: 24.19,
+    },
+    {
+        name: "NEW DIVIDE",
+        img: "data/discs/images/new_divide.jpg",
+        type: "SINGLE",
+        //song: "data/discs/songs/castle_of_glass.mp3",
+        price: 6.25,
+    }
+]
