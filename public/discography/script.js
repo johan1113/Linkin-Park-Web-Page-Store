ffb2c92a7809f7d6acad0b2b4a8dd419a9e3b44c
BurbanoJaime <burbanojaime98@gmail.com>
2018-10-27 15:26:40 -0500

    se agrega pintado dinamico de los detalles de los discos y su rrproduccion

--- public/discography/script.js
+++ public/discography/script.js
@@ -25,8 +25,50 @@ for (let index = 0; index < buttons.length; index++) {
       method: 'GET', // or 'PUT'
     }).then(res => res.json())
     .catch(error => console.error('Error:', error))
-    .then(response => console.log('Success:', response));
+    .then(disc => createDiscPresentation(disc));
   });
+}
+
+function createDiscPresentation(disc){
+  var section = document.createElement('section');
+  section.setAttribute('class', 'discpresentation');
+  
+  var btnClose = document.createElement('a');
+  btnClose.setAttribute('class', 'discpresentation__close');
+  btnClose.setAttribute('id', 'close');
+  btnClose.innerHTML = "X";
+  btnClose.addEventListener('click',function(e){
+    e.preventDefault();
+    section.remove();
+  });
+
+  var div = document.createElement('div');
+  div.setAttribute('class', 'discpresentation__reproductor');
+
+  var img =document.createElement('img');
+  img.setAttribute('class', 'discpresentation__reproductor__image');
+  img.setAttribute('src', '../'+disc.img);
+
+  var frame = document.createElement('iframe');
+  frame.setAttribute('class', 'discpresentation__reproductor__song');
+  frame.setAttribute('src',disc.url);
+  frame.setAttribute('frameborder','0');
+  frame.setAttribute('allowtransparency','true');
+  frame.setAttribute('allow','encrypted-media');
+
+  var btnAddCart =document.createElement('a');
+  btnAddCart.setAttribute('class', 'discpresentation__addcart');
+  btnAddCart.setAttribute('id', 'button');
+  btnAddCart.innerHTML = "ADD TO CART";
+
+  div.appendChild(img);
+  div.appendChild(frame);
+
+  section.appendChild(btnClose);
+  section.appendChild(div);
+  section.appendChild(btnAddCart);
+
+  document.getElementsByTagName('body')[0].appendChild(section);
 }
 
 
